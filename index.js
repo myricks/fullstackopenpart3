428c5c15f5c0b8492fd00d2fe26f896e43dbddb5
joonas <joonas.ylisaari@gmail.com>
2022-05-18 18:05:57 +0300

    3.13 & 3.14 puhelinluettelo ja tietokanta

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
+const Person = require('./models/person.js');
 app.use(cors())
 app.use(express.static('build'))
 
@@ -17,28 +19,6 @@ morgan.token('body', function getBody(req) {
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        id: 1,
-        name: 'Arto Hellas',
-        number: '040-123456'
-    },
-    {
-        id: 2,
-        name: 'Ada Lovelace',
-        number: '39-44-5323523'
-    },
-    {
-        id: 3,
-        name: 'Dan Abramov',
-        number: '12-43-234345'
-    },
-    {
-        id: 4,
-        name: 'Mary Poppendick',
-        number: '39-23-6423122'
-    }
-]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello world!</h1>')
@@ -50,7 +30,11 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({})
+        .then(persons => {
+            response.json(persons);
+        })
+
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -66,7 +50,6 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons', (request, response) => {
-    const id = Math.floor(Math.random() * 50000000);
     const body = request.body;
 
     if (!body.name || !body.number) {
@@ -75,19 +58,15 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.find(person => person.name === body.name)) {
-        response.status(400).json({
-            error: 'Name is already in the phonebook'
-        })
-    }
-    const person = {
-        id: id,
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
+    person.save().then(person => {
+        console.log('person added to phone book');
+        response.json(person);
+    })
 
-    persons = persons.concat(person);
-    response.json(person);
 })
 
 const PORT = process.env.PORT || 3001;
